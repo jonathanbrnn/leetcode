3c64e26a269d6c5ade2560b6b68213557d0bf4a3
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-03-09 18:02:40 +0100

    09.03.2024 DAILY PUZZLE Minimum Common Value (EASY)

---
+++ Two Sum/sol.cs
@@ -0,0 +1,9 @@
+class Solution() {
+    private array input = [1, 4, 10, 11, 3, 8]
+    private int target = 9
+    void Main(list) {
+        int left = 0
+        int right = list.getLength()
+        while left < right:
+    }
+}
