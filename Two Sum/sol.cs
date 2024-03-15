f3f08d616fab6d7d7ff1d2a9209f5c6722ea5503
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-03-15 16:52:39 +0100

    15.03.2024 DAILY PUZZLE Product of Array Except Self (MEDIUM)

--- Two Sum/sol.cs
+++ Two Sum/sol.cs
@@ -1,9 +1,39 @@
-class Solution() {
-    private array input = [1, 4, 10, 11, 3, 8]
-    private int target = 9
-    void Main(list) {
-        int left = 0
-        int right = list.getLength()
-        while left < right:
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    private List<int> input = new List<int> { 2, 4, 10, 11, 3, 8 };
+    private int target = 9;
+
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        solution.FindPairs();
+    }
+
+    public void FindPairs()
+    {
+        input.Sort();
+        int left = 0;
+        int right = input.Count - 1;
+        while (left < right)
+        {
+            int current = input[left] + input[right];
+            if (current == target)
+            {
+                Console.WriteLine(input[left] + ", " + input[right]);
+                return;
+            }
+            else if (current < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+        Console.WriteLine("None found");
     }
 }
