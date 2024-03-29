7b15c9b829b4358ede3cab411745856b28f2e014
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-03-29 23:08:19 +0100

    Count Subarrays WhereMax Element Appears at Least K Times (FIXED)

--- Count Subarrays Where Max Element Appears at Least K Times/sol.py
+++ Count Subarrays Where Max Element Appears at Least K Times/sol.py
@@ -1,20 +1,22 @@
 from collections import Counter
 
 class Solution:
-    def maxSubarrayLength(self, nums: List[int], k: int) -> int:
-        freq = {}
+    def countSubarrays(self, nums: list[int], k: int) -> int:
         n = len(nums)
+        max_num = max(nums)
         ans = 0
-        i = 0
+        count = 0
         j = 0
-        while j < n:
-            freq[nums[j]] = freq.get(nums[j], 0) + 1
-            while freq[nums[j]] > k:
-                freq[nums[i]] -= 1
-                i += 1
-            ans = max(ans, j - i + 1)
-            j += 1
+        for i in range(n):
+            if nums[i] == max_num:
+                count += 1
+            while count >= k:
+                if nums[j] == max_num:
+                    count -= 1
+                j += 1
+            ans += j
         return ans
+
 
 
 nums = [2,2,2,2,1,3,3,2,2,1,1,3,1,1,2,3,2,1,1,2,1,1,2,1,2,1,2,1,3,1,3,3]
