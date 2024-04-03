5e08162f1ccfb383a1bb9fb7c8b62eebe9b54998
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-04-03 15:12:21 +0200

    03.04.2024 Longest Palindrome (EASY) [38ms, 16.71mb]

---
+++ Longest Palindrome/sol.py
@@ -0,0 +1,21 @@
+from collections import Counter
+
+class Solution:
+    def longestPalindrome(self, s):
+        total = 0
+        has_odd = False
+        for val in Counter(s).values():
+            if val % 2 == 0:
+                total += val
+            else:
+                total += val - 1
+                has_odd = True
+
+        if has_odd:
+            total += 1
+
+        return total
+
+
+s = "civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"
+print(Solution().longestPalindrome(s))
