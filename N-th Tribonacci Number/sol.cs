d893673b952eae0c71cd37828a2ba3ac1a102094
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-04-25 21:02:54 +0200

    25.04.2024 Number of 1 Bits (MEDIUM) [31ms , 16.60mb]

---
+++ N-th Tribonacci Number/sol.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int Tribonacci(int n) { 
+        List<int> t = new List<int> {}
+        int i = 0;
+        int j = 3;
+        while (n > 2)
+        {
+            t
+        }
+    }
+}
