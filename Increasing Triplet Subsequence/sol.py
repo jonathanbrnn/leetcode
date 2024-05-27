85869ad945a90775b10c9b3462fd6c5c69500cf0
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-05-27 17:09:11 +0200

    Increasing Triplet Subsequence (MEDIUM) [764ms, 32.39mb]

---
+++ Increasing Triplet Subsequence/sol.py
@@ -0,0 +1,15 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        if len(set(nums)) < 3:
+            return False
+
+        first = second = float("inf")
+
+        for num in nums:
+            if num <= first:
+                first = num
+            elif num <= second:
+                second = num
+            else:
+                return True
+        return False
