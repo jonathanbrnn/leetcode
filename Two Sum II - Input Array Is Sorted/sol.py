2ef24687efaf26a0e3ab9f02d6c6a439266d3d46
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-08-02 21:01:36 +0200

    Two Sum II - Input Array Is Sorted (MEDIUM) [107ms, 17.66mb]

---
+++ Two Sum II - Input Array Is Sorted/sol.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            if nums[left] + nums[right] == target:
+                return [left + 1, right + 1]
+            if nums[left] + nums[right] < target:
+                left += 1
+            else:
+                right -= 1
