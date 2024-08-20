8494ee463a63f5cefdc15280028e94e0f2678dce
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-08-20 12:25:19 +0200

    Find Median from Data Stream (HARD) [3140ms, 38.86mb]

---
+++ HARD/Find Median from Data Stream (HARD)/sol.py
@@ -0,0 +1,22 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.stream = []
+
+    def addNum(self, num: int) -> None:
+        self.stream.append(num)
+
+    def findMedian(self) -> float:
+        length_stream = len(self.stream)
+        self.stream = sorted(self.stream)
+
+        if length_stream % 2 == 0:
+            return (self.stream[length_stream // 2 - 1] + self.stream[length_stream // 2]) / 2
+        else:
+            return (self.stream[int(length_stream / 2 - 0.5)])
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
