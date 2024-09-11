2a6fddb093ed1284fec2e161f467a7080b3e58da
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-09-11 16:57:02 +0200

    Counting Primes (MEDIUM) [1220ms, 55.15mb] - solved using the Sieve of Eratosthenes algorithm

--- MEDIUM/Count Primes (MEDIUM)/sol.py
+++ MEDIUM/Count Primes (MEDIUM)/sol.py
@@ -1,16 +1,17 @@
+# This an implementation of the Sieve of Eratosthenes algorithm, which I fund during research of prime numbers
+# For a great explenation, check out geeksforgeeks at https://www.geeksforgeeks.org/sieve-of-eratosthenes/
+
 class Solution:
     def countPrimes(self, n: int) -> int:
-        sol = 0
-        for i in range(2, n):
-            flag = False
-            for j in range(2, i):
-                if i % j == 0:
-                    flag = True
-                    break
-            if not flag:
-                sol += 1
-        return sol
+        if n <= 2:
+            return 0
 
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False
 
-sol = Solution()
-print(sol.countPrimes(499979))
+        for i in range(2, int(n ** 0.5) + 1):
+            if is_prime[i]:
+                for j in range(i * i, n, i):
+                    is_prime[j] = False
+
+        return sum(is_prime)
