9755d0386e5c86724e721d0f592d96256989bb90
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-09-18 15:25:01 +0200

    Forgot to save the file before commiting :/

--- MEDIUM/My Calendar I (MEDIUM)/sol.py
+++ MEDIUM/My Calendar I (MEDIUM)/sol.py
@@ -5,23 +5,19 @@ class MyCalendar:
 
     def book(self, start: int, end: int) -> bool:
         if self.events == []:
-            self.events.append(start)
-            self.events.append(end)
+            self.events.append((start, end))
             return True
         else:
             if start >= self.events[-1][1]:
-                self.events.append(start)
-                self.events.append(end)
+                self.events.append((start, end))
                 return True
-            elif end <= self.events[0]:
-                self.events.append(start)
-                self.events.append(end)
+            elif end <= self.events[0][0]:
+                self.events.append((start, end))
                 self.events.sort()
                 return True
             for i in range(len(self.events) - 1):
-                if self.events[i] <= start < self.events[i + 1] and self.events[i + 1] >= end:
-                    self.events.append(start)
-                    self.events.append(end)
+                if self.events[i][1] <= start < self.events[i + 1][0] and self.events[i + 1][0] >= end:
+                    self.events.append((start, end))
                     self.events.sort()
                     return True
             return False
