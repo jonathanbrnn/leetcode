07186d42937f1cfb3b6bc23a387c4c6275f4676b
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2024-11-20 17:49:38 +0100

    Sort Colors (MEDIUM) [0ms, 16.60mb] - the old solution did not adhere to the problem statement requirements

--- MEDIUM/Sort Colors (MEDIUM)/sol.py
+++ MEDIUM/Sort Colors (MEDIUM)/sol.py
@@ -1,4 +1,14 @@
-# IN ONE LINE AND IT BEATS 91% OF USERS:
+# This solution isn't great, but it now actually complies with the problem statement's requirements (the previous solution used the built-in sorting function :/).
 class Solution:
-    def sortColors(self, nums: list[int]):
-        nums = nums.sort()
+    def sortColors(self, nums: list) -> None:
+        count = [0, 0, 0]
+
+        for num in nums:
+            count[num] += 1
+
+        sol = [0] * count[0] + [1] * count[1] + [2] * count[2]
+
+        for i in range(len(nums)):
+            nums[i] = sol[i]
+
+        return nums
