7a49cac00a43f5fd26fd307ceea7a9e0c25b848d
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2025-12-01 15:04:50 +0100

    Palindrome Number (EASY) [11ms, 17.7mb] - added a better mathematical solution instead of using a reversed str

--- EASY/Palindrome Number (EASY)/_solution.py
+++ EASY/Palindrome Number (EASY)/_solution.py
@@ -1,4 +1,22 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         s = str(x)
-        return s == ''.join(reversed(s))
+        return s == "".join(reversed(s))
+
+
+# Better solution:
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
+        original: int = x
+        reverse: int = 0
+
+        while x > 0:
+            digit = x % 10
+            reverse = (reverse * 10) + digit
+
+            x //= 10
+
+        return original == reverse
