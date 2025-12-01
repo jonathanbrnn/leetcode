144409fac75e622c74a11cb30ff6255cec4e9435
Jonathan Braun <65763166+Luvox47@users.noreply.github.com>
2025-12-01 14:55:19 +0100

    Find the Pivot Integer (EASY) [0ms, 17.78mb] - O(n) -> O(1) by sqrt(int(n * (n + 1) // 2)) == n * (n + 1) // 2

--- EASY/Find the Pivot Integer (EASY)/sol.py
+++ EASY/Find the Pivot Integer (EASY)/sol.py
@@ -1,8 +1,8 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
         current_sum = 0
-        total_sum = sum(range(1, n+1))
-        for i in range(1, n+1):
+        total_sum = sum(range(1, n + 1))
+        for i in range(1, n + 1):
             current_sum += i
             if current_sum == total_sum:
                 return i
@@ -13,3 +13,17 @@ class Solution:
 
 sol = Solution()
 print(sol.pivotInteger(1))
+
+
+# BETTER SOLUTION
+
+
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        total_sum = n * (n + 1) // 2
+        pivot = int(math.sqrt(total_sum))
+
+        if pivot * pivot == total_sum:
+            return pivot
+
+        return -1
